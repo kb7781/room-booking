211860f8ba533b0e693325f96f8938b0568fb180
Kunal Bothra <Kunalbothra7781@gmail.com>
2026-02-21 22:09:07 +0530

    Update constants to use non-null assertion
    Change ADMIN_EMAIL and ADMIN_PASSWORD to use non-null assertion.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const ADMIN_EMAIL = process.env.NEXT_PUBLIC_ADMIN_EMAIL || 'admin@university.edu';
-export const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_ADMIN_PASSWORD || 'admin123';
+export const ADMIN_EMAIL = process.env.NEXT_PUBLIC_ADMIN_EMAIL!;
+export const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_ADMIN_PASSWORD!;
